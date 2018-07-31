7d749b2307affefd0c25534bf1d88f454afe5772
zhengwenli <zhengwenli_95@163.com>
2018-07-31 17:12:28 +0800

    fixed

--- main/main.js
+++ main/main.js
@@ -1,10 +1,6 @@
 // Write your cade below:
-module.exports = function main() {
-    function main(a,b){
-        var toBe;
-        var toBe = a % b;
-        return toBe;
-        console.log(main(9,3));
-        console.log(main(11,4));
-}
+module.exports = function main(a,b) {
+    return a % b;
+    console.log(main(9,3));
+    console.log(main(11,4));
 };
