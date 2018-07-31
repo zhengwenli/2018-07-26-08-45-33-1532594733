6087f211c3f988a725ee1194b08785a0950d8593
zhengwenli <zhengwenli_95@163.com>
2018-07-31 16:39:48 +0800

    add remainder function

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
 // Write your cade below:
 module.exports = function main() {
-  
+    function remainder_number(a,b){
+        var toBe;
+        var toBe = a % b;
+        return toBe;
+        console.log(remainder_number(9,3));
+        console.log(remainder_number(11,4));
+}
 };
